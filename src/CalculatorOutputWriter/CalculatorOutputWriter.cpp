d9e70a1a4f7b2544ad967f8ade038e1b1a67ff86
Дмитрий Воронин <voronin@mzta.ru>
2026-05-18 15:05:29 +0300

    Отступы

--- src/CalculatorOutputWriter/CalculatorOutputWriter.cpp
+++ src/CalculatorOutputWriter/CalculatorOutputWriter.cpp
@@ -16,7 +16,7 @@ void CalculatorOutputWriter::writeRequest(std::unique_ptr<CalculatorRequest>& ca
         return;
     }
 
-    QString logMessage("<span style='color: blue;'>Отправили запрос с ID: %1\n"
+    QString logMessage("<span style='color: blue;'>Отправили запрос с ID: %1<br>"
                                                   "Выражение: %2</span>");
     logMessage = logMessage.arg(calculatorRequest->getID()).arg(calculatorRequest->getExpression());
 
@@ -32,16 +32,16 @@ void CalculatorOutputWriter::writeResponse(std::unique_ptr<CalculatorResponse>& 
 
     if(calculatorResponse->isError())
     {
-        QString logMessage("<span style='color: yellow;'>Приняли ответ с ID: %1\n"
-                                                      "Ошибка: %2</span><br>");
+        QString logMessage("<span style='color: yellow;'>Приняли ответ с ID: %1<br>"
+                                                      "Ошибка: %2</span><br><br>");
         logMessage =  logMessage.arg(calculatorResponse->getID()).arg(calculatorResponse->getErrorString());
 
         emit sendLogMessage(logMessage);
     }
     else
     {
-        QString logMessage("<span style='color: green;'>Приняли ответ с ID: %1\n"
-                                                      "Результат: %2</span><br>");
+        QString logMessage("<span style='color: green;'>Приняли ответ с ID: %1<br>"
+                                                      "Результат: %2</span><br><br>");
         logMessage = logMessage.arg(calculatorResponse->getID()).arg(calculatorResponse->getResultCalculation());
 
         emit sendLogMessage(logMessage);
